ffe19de1f563e3d2687a72da5965dfc92dfb108f
ANDREU-Sarah-2326005aa <sarah.andreu@etu.univ-amu.fr>
2024-06-03 15:28:33 +0200

    Modification du nom des dossiers

--- src/main/java/com/example/echecsae/ChessController.java
+++ src/main/java/com/example/echecsae/ChessController.java
@@ -3,12 +3,12 @@ package com.example.echecsae;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ChessController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to Chess.com");
     }
 }
