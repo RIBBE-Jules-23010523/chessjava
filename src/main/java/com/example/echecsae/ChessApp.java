de2e172ab2ef26e650c588c1bd50eb9b136a9eb4
RIBBE-Jules-23010523 <jules.ribbe@etu.univ-amu.fr>
2024-06-04 13:42:24 +0200

    divers changements sur l'interface graphique

--- src/main/java/com/example/echecsae/ChessApp.java
+++ src/main/java/com/example/echecsae/ChessApp.java
@@ -7,11 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
 public class ChessApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChessApp.class.getResource("ChessView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 3000, 7000);
+        stage.setResizable(false);
         stage.setTitle("Chess");
         stage.setScene(scene);
         stage.show();
