1c2270cad450a2fffc9e5b5dc71c08c0fc7b19cb
Sim Dau <simon.daubrege@etu.univ-amu.fr>
2024-06-08 09:24:10 +0200

    ré implémentation du fichier PreGameFxml qui avait disparu et mise à jour du ChessController

--- src/main/java/com/example/echecsae/ChessController.java
+++ src/main/java/com/example/echecsae/ChessController.java
@@ -4,7 +4,9 @@ import com.example.echecsae.game.ChessPlate;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -19,6 +21,18 @@ public class ChessController {
 
     @FXML
     private Label joueur1;
+
+    @FXML
+    private Label joueur2;
+
+    @FXML
+    private TextField j_name1;
+
+    @FXML
+    private TextField j_name2;
+
+    @FXML
+    private Button play_button;
 
     @FXML
     public void initialize() {
@@ -69,6 +83,26 @@ public class ChessController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    @FXML
+    public void handlePreGameClick(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("PreGameView.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) joueur1.getScene().getWindow();
+            stage.setScene(new javafx.scene.Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void handleNameUpdateClick(MouseEvent event){
+
+        joueur1.setText(j_name1.getText());
+        joueur2.setText(j_name2.getText());
+
+        play_button.setDisable(true);
     }
 
 }
